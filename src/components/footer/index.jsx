63b0f91093f039a5c7e7fccc82ed5c4c738e7435
francisco-JGC <jjjchico1@gmail.com>
2024-07-27 12:50:17 -0600

    feat: Update footer with copyright information and apology message

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,5 +1,24 @@
 import './index.scss'
+import { SectionPage } from '../LayoutSectionPage'
 
 export const Footer = () => {
-  return <footer>foo</footer>
+  const year = new Date().getFullYear()
+
+  return (
+    <SectionPage className="footer">
+      <footer>
+        <p>
+          © {year} - Burguer. Todos los derechos reservados, Hecho por{' '}
+          <a
+            href="https://github.com/francisco-JGC"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Francisco Garcia (Chico Dev 😎)
+          </a>
+        </p>
+        <small>Disculpenme por el footer, espero no haya problema 😣❤️</small>
+      </footer>
+    </SectionPage>
+  )
 }
