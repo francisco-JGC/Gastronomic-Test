93ea28ac779652b0b6b5bded4ae318752643c08d
francisco-JGC <jjjchico1@gmail.com>
2024-07-27 11:26:11 -0600

    feat: Update SectionPage component to support optional blur effect

--- src/components/LayoutSectionPage/index.jsx
+++ src/components/LayoutSectionPage/index.jsx
@@ -1,9 +1,13 @@
 import './index.scss'
 
-export const SectionPage = ({ children, className }) => {
+export const SectionPage = ({ children, className, noBlur }) => {
   return (
     <div className={`section-page`}>
-      <section className={`section-page-content ${className}`}>
+      <section
+        className={`section-page-content ${className}
+      ${noBlur && `section-page-content__blur`}
+      `}
+      >
         {children}
       </section>
     </div>
