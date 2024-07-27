d89186bb3321a1ed122bcdb3c900167d6ebda89e
francisco-JGC <jjjchico1@gmail.com>
2024-07-27 12:23:52 -0600

    feat: Add MenuSection component to home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,12 @@
 import { HomeSection } from './components/HomeSection'
+import { MenuSection } from './components/MenuSection'
 import './index.scss'
 
 export const HomePage = () => {
   return (
-    <div>
+    <div className="home-page">
       <HomeSection />
+      <MenuSection />
     </div>
   )
 }
