a12021d2f4065e717789885e52eddde08327aca8
francisco-JGC <jjjchico1@gmail.com>
2024-07-27 12:23:48 -0600

    feat: Add burger menu items with lazy loading for images

---
+++ src/constants/burguerMenu.js
@@ -0,0 +1,67 @@
+import bigmac from '../assets/images/big-mac.webp'
+import cheeseburguer from '../assets/images/cheeseburguer.webp'
+import classic from '../assets/images/classic.webp'
+import doublecheese from '../assets/images/double-cheese.webp'
+import double from '../assets/images/double.webp'
+import quearterPounder from '../assets/images/quarter-pounder.webp'
+import quearterPounderDeluxe from '../assets/images/quarter-pounder-deluxe.webp'
+import doubleQuearterPounder from '../assets/images/double-quarter-pounder.webp'
+
+export const BURGUERMENU = [
+  {
+    title: 'Big Mac',
+    price: 6.5,
+    image: bigmac,
+    description:
+      'Doble carne, doble queso, cebolla, pepinillos, lechuga, salsa especial y pan con semillas de sésamo.',
+  },
+  // {
+  //   title: 'Cheeseburguer',
+  //   price: 5.5,
+  //   image: cheeseburguer,
+  //   description:
+  //     'Carne, queso, cebolla, pepinillos, mostaza y ketchup en un pan con semillas de sésamo.',
+  // },
+  // {
+  //   title: 'Classic',
+  //   price: 5.5,
+  //   image: classic,
+  //   description:
+  //     'Carne, cebolla, pepinillos, mostaza y ketchup en un pan con semillas de sésamo.',
+  // },
+  {
+    title: 'Double Cheese',
+    price: 6.5,
+    image: doublecheese,
+    description:
+      'Doble carne, doble queso, cebolla, pepinillos, mostaza y ketchup en un pan con semillas de sésamo.',
+  },
+  {
+    title: 'Double',
+    price: 6.5,
+    image: double,
+    description:
+      'Doble carne, cebolla, pepinillos, mostaza y ketchup en un pan con semillas de sésamo.',
+  },
+  {
+    title: 'Quearter Pounder',
+    price: 6.5,
+    image: quearterPounder,
+    description:
+      'Carne, cebolla, pepinillos, mostaza y ketchup en un pan con semillas de sésamo.',
+  },
+  {
+    title: 'Quearter Pounder Deluxe',
+    price: 7.5,
+    image: quearterPounderDeluxe,
+    description:
+      'Carne, cebolla, pepinillos, mostaza y ketchup en un pan con semillas de sésamo.',
+  },
+  {
+    title: 'Double Quearter Pounder',
+    price: 8.5,
+    image: doubleQuearterPounder,
+    description:
+      'Doble carne, cebolla, pepinillos, mostaza y ketchup en un pan con semillas de sésamo.',
+  },
+]
