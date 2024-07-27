15266219c5f010b5da12c0aeae3ea9f5f454643d
francisco-JGC <jjjchico1@gmail.com>
2024-07-27 01:15:19 -0600

    refactor: Update navbar component to include logo, menu items, and order button

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,20 +1,39 @@
 import './index.scss'
 import { ROUTES } from '../../configurations/router.conf'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo"></div>
+      <div className="navbar-logo">
+        <Link to="/" className="navbar-link">
+          Burguers.
+        </Link>
+      </div>
       <ul className="navbar-menu">
         {ROUTES.map((item, index) => (
           <li key={index} className="navbar-item">
-            <Link to={item.path} className="navbar-link">
+            <NavLink
+              to={item.path}
+              className={({ isActive }) =>
+                isActive ? 'navbar-link navbar-link-active' : 'navbar-link'
+              }
+            >
               {item.title}
-            </Link>
+            </NavLink>
           </li>
         ))}
+
+        <li className="navbar-item">
+          <a href="tel:+1234567890" className="navbar-link">
+            + 1 234 567 890
+          </a>
+        </li>
       </ul>
+
+      <div className="navbar-order">
+        <button>Ordenar Ahora</button>
+      </div>
     </nav>
   )
 }
